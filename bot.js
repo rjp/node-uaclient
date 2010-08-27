51e372c556abcefe26a02cd17c374f097c3930ba
rjp <zimpenfish@gmail.com>
2010-08-27 19:07:48 +0100

    - no need for our own subclass if we use events instead of methods

--- bot.js
+++ bot.js
@@ -1,18 +1,11 @@
 var sys = require('sys');
 var uaclient = require('uaclient');
 
-function Bot() {
-    uaclient.UAClient.call(this);
-    var self = this;
-}
-sys.inherits(Bot, uaclient.UAClient);
-
-larabot = new Bot;
+larabot = new uaclient.UAClient;
 function announce_user_page(a) {
     larabot.flatten(a);
     sys.puts("= paged by "+a["fromname"]+"/"+a["fromid"]+", ``"+a["text"]+"''");
     larabot.page(a["fromid"], "thank you for paging larabot, have a nice day");
 }
-larabot.removeAllListeners('announce_user_page'); // nonce
 larabot.addListener("announce_user_page", announce_user_page);
 larabot.connect(process.argv[2], process.argv[3]);
