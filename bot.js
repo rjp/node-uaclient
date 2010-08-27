30fd5222e865048d2a551f9a0284b88b76060c60
rjp <zimpenfish@gmail.com>
2010-08-27 19:02:49 +0100

    - move bot.js to handle events from uaclient.js, not methods

--- bot.js
+++ bot.js
@@ -8,9 +8,11 @@ function Bot() {
 sys.inherits(Bot, uaclient.UAClient);
 
 larabot = new Bot;
-Bot.prototype.announce_user_page = function(a) {
+function announce_user_page(a) {
     larabot.flatten(a);
     sys.puts("= paged by "+a["fromname"]+"/"+a["fromid"]+", ``"+a["text"]+"''");
     larabot.page(a["fromid"], "thank you for paging larabot, have a nice day");
 }
+larabot.removeAllListeners('announce_user_page'); // nonce
+larabot.addListener("announce_user_page", announce_user_page);
 larabot.connect(process.argv[2], process.argv[3]);
