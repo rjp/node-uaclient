b03e5f2304196586209c5d711214f0ad971a4aa5
rjp <zimpenfish@gmail.com>
2011-05-15 14:41:15 +0100

    - checkpoint before major rewrite

--- examples/bot.js
+++ examples/bot.js
@@ -8,5 +8,8 @@ function announce_user_page(a) {
     larabot.page(a["fromid"], "thank you for paging larabot, have a nice day");
     sys.puts("check: "+a["fromname"]+" => "+a["fromid"]+" / "+sys.inspect(larabot.get_user(a["fromname"])));
 }
+larabot.shadow = 256;
+larabot.caching = false;
 larabot.addListener("announce_user_page", announce_user_page);
-larabot.connect(process.argv[2], process.argv[3]);
+larabot.addListener("uaclient_login", function(){sys.puts("active session");});
+larabot.connect(process.argv[2], process.argv[3], process.argv[4], process.argv[5]);
